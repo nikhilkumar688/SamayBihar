70463c8cdb164b6791443fb1ba75fda5d3fc316d
Nikhil Kumar <your_email@example.com>
2025-07-09 02:57:53 +0530

    created vite project with tailwind css

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <Button className="bg-red-500" variant="ghost">
+        Click me
+      </Button>
+    </div>
+  );
+};
+
+export default App;
