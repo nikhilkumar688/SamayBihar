a29ee1f64a9e38eed0e3330341cb893171c34cf4
Nikhil Kumar <your_email@example.com>
2025-07-15 09:22:06 +0530

    add pages and route

---
+++ frontend/src/auth/forms/SignInForm.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SignInForm = () => {
+  return <div>SignInForm</div>;
+};
+export default SignInForm;
---
+++ frontend/src/auth/forms/SignUpForm.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const SignUpForm = () => {
+  return <div>SignUpForm</div>;
+};
+export default SignUpForm;
