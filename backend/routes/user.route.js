a8470232c33fcfc570ddb2bb11a1fd876cf486aa
Nikhil Kumar <your_email@example.com>
2025-07-16 16:04:43 +0530

    updated delete user api route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,6 +1,8 @@
 import express from "express";
-import { updateUser } from "../controllers/user.controller.js";
+import { deleteUser, updateUser } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 router.put("/update/:userId", verifyToken, updateUser);
+router.put("/delete/:userId", verifyToken, deleteUser);
+
 export default router;
