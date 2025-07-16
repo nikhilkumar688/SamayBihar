72ca14ff3adc333e7bb5b96b174c26898ff0ed93
Nikhil Kumar <your_email@example.com>
2025-07-16 11:18:35 +0530

    Add update user api route

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authRoutes from "./routes/auth.route.js";
+import userRoutes from "./routes/user.route.js";
+
 dotenv.config();
 mongoose
   .connect(process.env.MONGO_URI)
@@ -14,10 +17,12 @@ mongoose
 const app = express();
 //for allowing json object in req body
 app.use(express.json());
+app.use(cookieParser());
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
 app.use("/api/auth", authRoutes);
+app.use("/api/user", userRoutes);
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
